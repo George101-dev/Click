0b5b90c28ae3bbf8557c134b5e1464d8e1868052
George101-dev <georgekiroff50@gmail.com>
2026-02-21 15:07:17 +0200

    Update upgrade_coins_on_click.gd

--- upgrade_coins_on_click.gd
+++ upgrade_coins_on_click.gd
@@ -1,6 +1,17 @@
 extends Area2D
 
+var pressCheck = false
+var coins = 0
+var upgradeCost = 100
+var n = 1
+
 func _input_event(iewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
+		if pressCheck == false:
+			$AnimatedSprite2D.play("Idle")
+			return;
 		if event.is_action_pressed('Click'):
-			$AnimatedSprite2D.play("Press");
+			$AnimatedSprite2D.play("Press")
+			
+			
+			
