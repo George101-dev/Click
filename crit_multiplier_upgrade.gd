ba2d4bf104621d594aaef7f154e0fc282ebf40da
George101-dev <georgekiroff50@gmail.com>
2026-03-12 16:37:56 +0200

    crit_multiplier_upgrade1

---
+++ crit_multiplier_upgrade.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+signal upgrade_crit
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			$AnimatedSprite2D.play("Press")
+			$ClickSound.play()
+			await get_tree().create_timer(0.3).timeout
+			$AnimatedSprite2D.play("Idle")
+			emit_signal("upgrade_crit")
