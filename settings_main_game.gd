43fd6c59151e00eabfbaa52b7bb8593734c6aab5
George101-dev <georgekiroff50@gmail.com>
2026-06-10 16:15:28 +0300

    fullScreenAndMenuUpgrade

--- settings_main_game.gd
+++ settings_main_game.gd
@@ -6,3 +6,15 @@ var buttonType = null
 
 func _on_pressed() -> void:
 	$OptionsMenu.show()
+
+
+func _on_button_2_pressed() -> void:
+	buttonType = "toMenu"
+	$Fade_transition.show()
+	$Fade_transition/Fade_Timer.start()
+	$Fade_transition/AnimationPlayer.play("fade_in")
+
+
+func _on_fade_timer_timeout() -> void:
+	if buttonType == "toMenu":
+		get_tree().change_scene_to_file("res://main_menu.tscn")
