2957fadb764645d6d51da377b4013e425edffc12
George101-dev <georgekiroff50@gmail.com>
2026-06-12 18:25:22 +0300

    FullscreenButton

---
+++ fullscreen_checkbox.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	
