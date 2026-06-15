830bc8a05ccc7fcf34664471be18489025aa94a0
George101-dev <georgekiroff50@gmail.com>
2026-06-15 14:32:29 +0300

    Update fullscreen_checkbox.gd

--- fullscreen_checkbox.gd
+++ fullscreen_checkbox.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-
+func _ready() -> void:
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		$CheckButton.button_pressed = true
+	else:
+		$CheckButton.button_pressed = false
 
 func _on_check_button_toggled(toggled_on: bool) -> void:
 	if toggled_on == true:
